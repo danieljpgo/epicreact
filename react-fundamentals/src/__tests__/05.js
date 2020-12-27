bf1f79260833bb9f74e8ea90b885653e3c076e7d
Daniel Jorge <djpgonzaga@gmail.com>
2020-12-27 18:08:40 -0300

    feat(react fundamentals): Styling

--- react-fundamentals/src/__tests__/05.js
+++ react-fundamentals/src/__tests__/05.js
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import chalk from 'chalk'
-import {render, screen, prettyDOM} from '@testing-library/react'
-import App from '../final/05'
-// import App from '../exercise/05'
+import { render, screen, prettyDOM } from '@testing-library/react'
+// import App from '../final/05'
+import App from '../exercise/05'
 
 test('renders the correct styles new', () => {
-  const {container} = render(<App />)
+  const { container } = render(<App />)
   const allBoxes = screen.getAllByText(/box/i)
 
   try {
