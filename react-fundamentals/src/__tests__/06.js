e0db2570451990f8fc1310a22918c8c0731b9dd4
Daniel Jorge <djpgonzaga@gmail.com>
2020-12-29 23:34:25 -0300

    feat(react fundamentals): Forms

--- react-fundamentals/src/__tests__/06.js
+++ react-fundamentals/src/__tests__/06.js
@@ -1,11 +1,11 @@
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import App from '../final/06'
-// import App from '../exercise/06'
+// import App from '../final/06'
+import App from '../exercise/06'
 
 beforeAll(() => {
-  jest.spyOn(global, 'alert').mockImplementation(() => {})
+  jest.spyOn(global, 'alert').mockImplementation(() => { })
 })
 
 beforeEach(() => {
--- react-fundamentals/src/exercise/06.js
+++ react-fundamentals/src/exercise/06.js
@@ -3,28 +3,35 @@
 
 import * as React from 'react'
 
-function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+function UsernameForm({ onSubmitUsername }) {
+  const [username, setUsername] = React.useState('');
+  const inputRef = React.useRef(null);
 
-  // 🐨 add the onSubmit handler to the <form> below
+  function handleSubmit(event, value) {
+    event.preventDefault();
+    onSubmitUsername(value);
+  }
 
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  function handleChange(value) {
+    setUsername(value.toLowerCase())
+  }
+
   return (
-    <form>
+    <form onSubmit={(event) => handleSubmit(event, inputRef.current.value)}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="username">Username:</label>
+        <input
+          id="username"
+          name="username"
+          type="text"
+          ref={inputRef}
+          value={username}
+          onChange={(event) => handleChange(event.target.value)}
+        />
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit">
+        Submit
+      </button>
     </form>
   )
 }
