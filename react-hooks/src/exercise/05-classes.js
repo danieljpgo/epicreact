85544c09e0984e9cebdd1f3027a3632c26b9007b
Daniel Jorge <djpgonzaga@gmail.com>
2021-01-08 23:12:25 -0300

    feat(react hooks): useRef and useEffect - DOM interaction

--- react-hooks/src/exercise/05-classes.js
+++ react-hooks/src/exercise/05-classes.js
@@ -8,29 +8,29 @@ import VanillaTilt from 'vanilla-tilt'
 // If you'd rather practice refactoring a class component to a function
 // component with hooks, then go ahead and do this exercise.
 
-class Tilt extends React.Component {
-  tiltRef = React.createRef()
-  componentDidMount() {
-    const tiltNode = this.tiltRef.current
-    const vanillaTiltOptions = {
+export function Tilt({ children }) {
+  const tiltRef = React.useRef();
+
+  React.useEffect(() => {
+    const tiltNode = tiltRef.current;
+
+    VanillaTilt.init(tiltNode, {
       max: 25,
       speed: 400,
       glare: true,
       'max-glare': 0.5,
-    }
-    VanillaTilt.init(tiltNode, vanillaTiltOptions)
-  }
-  componentWillUnmount() {
-    this.tiltRef.current.vanillaTilt.destroy()
-  }
-  render() {
-    return (
-      <div ref={this.tiltRef} className="tilt-root">
-        <div className="tilt-child">{this.props.children}</div>
-      </div>
-    )
-  }
+    })
+
+    return () => tiltNode.vanillaTilt.destroy();
+  }, []);
+
+  return (
+    <div ref={tiltRef} className="tilt-root">
+      <div className="tilt-child">{children}</div>
+    </div>
+  )
 }
+
 function App() {
   return (
     <Tilt>
