f28b00ea3a104179675efc608adc68579820c1f5
Daniel Jorge <djpgonzaga@gmail.com>
2021-01-30 21:58:06 -0300

    feat(advanced react hooks):  useContext - simple Counter - part 2/3

--- advanced-react-hooks/src/exercise/03.js
+++ advanced-react-hooks/src/exercise/03.js
@@ -2,38 +2,47 @@
 // http://localhost:3000/isolated/exercise/03.js
 
 import * as React from 'react'
+import {ErrorBoundary} from 'react-error-boundary'
 
-// 🐨 create your CountContext here with React.createContext
+const CountContext = React.createContext()
 
-// 🐨 create a CountProvider component here that does this:
-//   🐨 get the count state and setCount updater with React.useState
-//   🐨 create a `value` array with count and setCount
-//   🐨 return your context provider with the value assigned to that array and forward all the other props
-//   💰 more specifically, we need the children prop forwarded to the context provider
+const useCount = () => {
+  const context = React.useContext(CountContext)
+  if (!context) {
+    throw new Error(`useCount must be used within a CountProvider.`)
+  }
+  return context
+}
+
+const CountProvider = ({children}) => {
+  const [count, setCount] = React.useState(0)
+  const value = [count, setCount]
+  return <CountContext.Provider value={value}>{children}</CountContext.Provider>
+}
 
 function CountDisplay() {
-  // 🐨 get the count from useContext with the CountContext
-  const count = 0
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  // 🐨 get the setCount from useContext with the CountContext
-  const setCount = () => {}
+  const [, setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
 
 function App() {
   return (
-    <div>
-      {/*
-        🐨 wrap these two components in the CountProvider so they can access
-        the CountContext value
-      */}
-      <CountDisplay />
-      <Counter />
-    </div>
+    <ErrorBoundary
+      fallback={<div>useCount must be used within a CountProvider.</div>}
+    >
+      <div>
+        <CountProvider>
+          <CountDisplay />
+          <Counter />
+        </CountProvider>
+      </div>
+    </ErrorBoundary>
   )
 }
 
