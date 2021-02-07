928dc61cd07139de55b9c6991199d9663625640e
Daniel Jorge <djpgonzaga@gmail.com>
2021-02-07 18:28:42 -0300

    feat(advanced react hooks): useDebugValue: useMedia

--- advanced-react-hooks/src/exercise/06.js
+++ advanced-react-hooks/src/exercise/06.js
@@ -3,10 +3,13 @@
 
 import * as React from 'react'
 
+function formatedDebugValue({query, state}) {
+  return `\`${query}\` => ${state}`
+}
+
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
-  // 🐨 call React.useDebugValue here.
-  // 💰 here's the formatted label I use: `\`${query}\` => ${state}`
+  React.useDebugValue({query, state}, formatedDebugValue)
 
   React.useEffect(() => {
     let mounted = true
