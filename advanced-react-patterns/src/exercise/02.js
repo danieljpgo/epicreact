66f2eed7bae4231b50653d7e1f8c65fde15df63d
Daniel Jorge <djpgonzaga@gmail.com>
2021-07-11 12:40:54 -0300

    feat(advanced react patterns): compound components

--- advanced-react-patterns/src/exercise/02.js
+++ advanced-react-patterns/src/exercise/02.js
@@ -4,29 +4,24 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-function Toggle() {
+const ToggleOn = ({on, children}) => (on ? children : null)
+
+const ToggleOff = ({on, children}) => (on ? null : children)
+
+const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle} />
+
+const toggleTypes = [ToggleOn, ToggleOff, ToggleButton]
+
+function Toggle(props) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 replace this with a call to React.Children.map and map each child in
-  // props.children to a clone of that child with the props they need using
-  // React.cloneElement.
-  // 💰 React.Children.map(props.children, child => {/* return child clone here */})
-  // 📜 https://reactjs.org/docs/react-api.html#reactchildren
-  // 📜 https://reactjs.org/docs/react-api.html#cloneelement
-  return <Switch on={on} onClick={toggle} />
+  return React.Children.map(props.children, child =>
+    toggleTypes.includes(child.type)
+      ? React.cloneElement(child, {on, toggle})
+      : child,
+  )
 }
-
-// 🐨 Flesh out each of these components
-
-// Accepts `on` and `children` props and returns `children` if `on` is true
-const ToggleOn = () => null
-
-// Accepts `on` and `children` props and returns `children` if `on` is false
-const ToggleOff = () => null
-
-// Accepts `on` and `toggle` props and returns the <Switch /> with those props.
-const ToggleButton = () => null
 
 function App() {
   return (
@@ -34,6 +29,7 @@ function App() {
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <span>Hello</span>
         <ToggleButton />
       </Toggle>
     </div>
